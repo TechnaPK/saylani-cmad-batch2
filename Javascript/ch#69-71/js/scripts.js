e71cf459d7bb8213c8e500fd22bd9462afa68c06
Umar Ahmad <s.umarahmad@gmail.com>
2018-10-04 12:09:41 +0500

    Practice code for session 29/09/2018 added

---
+++ Javascript/ch#69-71/js/scripts.js
@@ -0,0 +1,64 @@
+//Blank Object
+var plan1 = {};
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var plan1 = {
+//     name: "Basic",
+//     price: 3.99,
+//     space: 100,
+//     transfer: 1000,
+//     pages: 10,
+//     discountMonths: [6, 7]
+// };
+
+
+
+
+
+
+
+//Property in Object
+// console.log( "price" in plan1 )
+
+
+
+
+
+
+
+
+// var plan1 = {
+
+//     name: "Basic",
+//     price: 3.99,
+//     space: 100,
+//     transfer: 1000,
+//     pages: 10,
+//     discountMonths: [6, 7],
+//     calcAnnual: function(){
+
+//         return this.price * 12;
+
+//     }
+
+// };
+
+
+
+
+
+
+
+
+
