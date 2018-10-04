c0d28d87ba5e37da2193a4d24277b6d7c983f414
Umar Ahmad <s.umarahmad@gmail.com>
2018-10-04 15:08:03 +0500

    Practice code for ch#72-75 added

---
+++ Javascript/ch#72-75/js/constructor.js
@@ -0,0 +1,21 @@
+function Plan(name, price, pages) {
+
+    this.name = name;
+    this.price = price;
+    this.pages = pages;
+
+    //Not a good way to define shared methods
+    // this.calcAnnual = function(){
+    //     return this.price * 12;
+    // }
+
+}
+
+//Good way to define shared methods
+Plan.prototype.calcAnnual = function () {
+    return this.price * 12;
+}
+
+var plan1 = new Plan("Basic", 3.99, 10)
+var plan2 = new Plan("Professional", 5.99, 50)
+var plan3 = new Plan("Ultimate", 9.99, 500)
---
+++ Javascript/ch#72-75/js/scripts.js
@@ -0,0 +1,32 @@
+var plan1 = {
+
+    name: "Basic",
+    price: 3.99,
+    pages: 10,
+    calcAnnual: function(){
+        return this.price * 12;
+    }
+
+};
+
+// var plan2 = {
+
+//     name: "Professional",
+//     price: 5.99,
+//     pages: 50,
+//     calcAnnual: function(){
+//         return this.price * 12;
+//     }
+
+// };
+
+// var plan3 = {
+
+//     name: "Ultimate",
+//     price: 9.99,
+//     pages: 500,
+//     calcAnnual: function(){
+//         return this.price * 12;
+//     }
+
+// };
